f71304dc01e7dd22ca31017eb81313366c12adb7
Micado Pierre <Biml16gb@gmail.com>
2024-01-04 10:11:51 -0500

    a

--- src/componenets/services/Services.jsx
+++ src/componenets/services/Services.jsx
@@ -2,15 +2,34 @@ import "./services.scss"
 import {motion} from "framer-motion"
 
 
+const variants = {
+    initial: {
+        x: -500,
+        y: 100,
+        opacity:0,
+    },
+    animate: {
+        x:0,
+        opacity:1,
+        y:0,
+        transition: {
+            duration: 1,
+            staggeredChildren: 0.1,
+        },
+    },
+}
+
+
+
 const Services = () => {
     return (
-        <motion.div className="services"> Services 
-        <motion.div className="textContainer">
+        <motion.div className="services" variants={variants} initial="intial" animate="animate"> Services 
+        <motion.div className="textContainer" variants={variants}>
             <p>I focus on helping you brand grow 
-            <br /> and invest in your future</p>
+            <br /> and investing in your future</p>
             <hr />
         </motion.div>
-        <motion.div className="TitleContainer">
+        <motion.div className="TitleContainer"  variants={variants}>
             <div className="title">
                 <img src="/peopl.webp" alt="" />
                 <h1> 
@@ -24,7 +43,7 @@ const Services = () => {
             </div>
             <button>WHAT WE DO?</button>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div className="listContainer"  variants={variants}>
             <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Branding</h2>
                 <p>Please add Info On Branding </p>

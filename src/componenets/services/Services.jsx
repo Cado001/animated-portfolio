58d6a730e2619f5e4eeeeddf4292e1cadb652361
Micado Pierre <Biml16gb@gmail.com>
2024-01-05 14:30:41 -0500

    fixed picture on services section

--- src/componenets/services/Services.jsx
+++ src/componenets/services/Services.jsx
@@ -1,17 +1,17 @@
 import "./services.scss"
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 
 const variants = {
     initial: {
         x: -500,
         y: 100,
-        opacity:0,
+        opacity: 0,
     },
     animate: {
-        x:0,
-        opacity:1,
-        y:0,
+        x: 0,
+        opacity: 1,
+        y: 0,
         transition: {
             duration: 1,
             staggeredChildren: 0.1,
@@ -23,57 +23,57 @@ const variants = {
 
 const Services = () => {
     return (
-        <motion.div className="services" variants={variants} initial="intial" animate="animate"> Services 
-        <motion.div className="textContainer" variants={variants}>
-            <p>I focus on helping you brand grow 
-            <br /> and investing in your future</p>
-            <hr />
-        </motion.div>
-        <motion.div className="TitleContainer"  variants={variants}>
-            <div className="title">
-                <img src="/peopl.webp" alt="" />
-                <h1> 
-               <b> Unique </b>  Ideas
-                 </h1>
-            </div>
-            <div className="title">
-                <h1> 
-               <b> For Your </b>  Business.
-                 </h1>
-            </div>
-            <button>WHAT WE DO?</button>
-        </motion.div>
-        <motion.div className="listContainer"  variants={variants}>
-            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
-                <h2>Branding</h2>
-                <p>Please add Info On Branding </p>
-                <button>Go</button>
+        <motion.div className="services" variants={variants} initial="intial" animate="animate"> Services
+            <motion.div className="textContainer" variants={variants}>
+                <p>I focus on helping you brand grow
+                    <br /> and investing in your future</p>
+                <hr />
             </motion.div>
-            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
-                <h2>Web and App Development</h2>
-                <p>Please add Info On Branding </p>
-                <button>Go</button>
+            <motion.div className="titleContainer" variants={variants}>
+                <div className="title">
+                    <img src="/people.png" alt="" />
+                    <h1>
+                        <b>Unique</b>  Ideas
+                    </h1>
+                </div>
+                <div className="title">
+                    <h1>
+                        <b> For Your </b>  Business.
+                    </h1>
+                </div>
+                <button>WHAT WE DO?</button>
             </motion.div>
-            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
-                <h2>Real Estate</h2>
-                <p>Please add Info On Branding </p>
-                <button>Go</button>
+            <motion.div className="listContainer" variants={variants}>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Branding</h2>
+                    <p>Please add Info On Branding </p>
+                    <button>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Web and App Development</h2>
+                    <p>Please add Info On Branding </p>
+                    <button>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Real Estate</h2>
+                    <p>Please add Info On Branding </p>
+                    <button>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Immigration Services</h2>
+                    <p>Please add Info On Branding </p>
+                    <button>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Property Mangement</h2>
+                    <p>Please add Info On Branding </p>
+                    <button>Go</button>
+                </motion.div>
             </motion.div>
-            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
-                <h2>Immigration Services</h2>
-                <p>Please add Info On Branding </p>
-                <button>Go</button>
-            </motion.div>
-            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
-                <h2>Property Mangement</h2>
-                <p>Please add Info On Branding </p>
-                <button>Go</button>
-            </motion.div>
-        </motion.div>
 
         </motion.div>
     )
-   
+
 }
 
 export default Services; 
